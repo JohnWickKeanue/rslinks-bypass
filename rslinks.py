f1978aab38174acde551b0d3501f1d7fdb60b938
Saurabh <61317144+html-css-js-art@users.noreply.github.com>
2023-05-06 20:49:05 +0530

    Update rslinks.py

--- rslinks.py
+++ rslinks.py
@@ -4,7 +4,7 @@ url = "https://rslinks.net/ds9ux2oe"
 
 def rs(url):
       client = requests.session()
-      download = get(url, stream=True, allow_redirects=False)
+      download = client.get(url, stream=True, allow_redirects=False)
       v = download.headers["location"]
       code = v.split('ms9')[-1]
       final = f"http://techyproio.blogspot.com/p/short.html?{code}=="
